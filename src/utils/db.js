0e10212fbfe36fc9eab424e1ce5889910db7de4f
ArefinShaon <arefinshaon99@gmail.com>
2023-08-21 00:23:56 +0600

    readme file added

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,16 +2,9 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO, {
-      useNewUrlParser: true, // Use the new URL parser
-      useUnifiedTopology: true, // Use the new server discovery and monitoring engine
-      useCreateIndex: true, // Ensure indexes are created automatically
-      useFindAndModify: false, // Use the new methods for find and modify
-    });
-    console.log("Connected to MongoDB");
+    await mongoose.connect(process.env.MONGO);
   } catch (error) {
-    console.error("Connection failed:", error.message);
-    throw error;
+    throw new Error("Connection failed!");
   }
 };
 
